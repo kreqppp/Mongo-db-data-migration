d0d0ecc8164ba28a08da9d92d5516db055234899
Petro Kostenyuk <p.kostenyuk@iqmessenger.com>
2022-11-21 22:34:54 +0200

    init commit

---
+++ src/main/java/com/kreqppp/mddm/MongodbDataMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.kreqppp.mddm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbDataMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbDataMigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kreqppp/mddm/MongodbDataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreqppp.mddm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbDataMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
